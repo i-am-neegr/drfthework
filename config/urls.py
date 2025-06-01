c920f2147b9c2c3ad2af4800e0692e597706d6f7
i-am-neegr <166641557+i-am-neegr@users.noreply.github.com>
2025-06-02 00:29:37 +0300

    small fixes

--- config/urls.py
+++ config/urls.py
@@ -32,3 +32,5 @@ urlpatterns = [
     path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
     path('lessons/', views.LessonListView.as_view(), name='lesson_list'),
 ]
+
+
